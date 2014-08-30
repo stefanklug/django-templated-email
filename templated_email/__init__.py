bcac9a9d912bb6d5fadcbf2f7058d7c585db5c73
Stefan Klug <klug.stefan@gmx.de>
2014-08-30 23:58:21 +0200

    python 3 doesn't have basestring anymore

--- templated_email/__init__.py
+++ templated_email/__init__.py
@@ -15,7 +15,7 @@ def get_connection(backend=None, template_prefix=None, template_suffix=None,
     """
     # This method is mostly a copy of the backend loader present in django.core.mail.get_connection
     klass_path = backend or getattr(settings, 'TEMPLATED_EMAIL_BACKEND', TemplateBackend)
-    if isinstance(klass_path, basestring):
+    if isinstance(klass_path, str):
         try:
             # First check if class name is omited and we have module in settings
             mod = import_module(klass_path)
